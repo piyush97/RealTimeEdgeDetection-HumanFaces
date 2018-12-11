c94b8069d07b895855c514864ae721ce591ef0e8
Piyush Mehta <me@piyushmehta.com>
2018-12-11 21:22:41 +0530

    Express server live

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require('express')
+const path = require('path')
+const { get } = require('request')
+
+const app = express()
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
