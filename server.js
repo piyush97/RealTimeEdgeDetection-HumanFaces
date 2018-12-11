816c812a59a9897191c7d951cf69e856f85d7145
Piyush Mehta <me@piyushmehta.com>
2018-12-11 21:32:16 +0530

    Server UP and Client UP

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Created with love by Piyush Mehta <me@piyushmehta.com>
 const express = require('express')
 const path = require('path')
 const { get } = require('request')
@@ -7,9 +8,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+// for front end views directory @define
 const viewsDir = path.join(__dirname, 'Client')
 app.use(express.static(viewsDir))
+// Public directory addn
 app.use(express.static(path.join(__dirname, './public')))
 
+app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'videoFaceTracking.html')))
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
