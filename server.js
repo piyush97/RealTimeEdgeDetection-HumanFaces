0d83347a8f7a2fe6db182202075659af3741a3c4
Piyush Mehta <me@piyushmehta.com>
2018-12-11 21:25:40 +0530

    path def

--- server.js
+++ server.js
@@ -4,4 +4,12 @@ const { get } = require('request')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const viewsDir = path.join(__dirname, 'Client')
+app.use(express.static(viewsDir))
+app.use(express.static(path.join(__dirname, './public')))
+
+
 app.listen(3000, () => console.log('Listening on port 3000!'));
